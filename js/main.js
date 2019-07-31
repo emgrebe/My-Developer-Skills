17bee203ed91dbd17b085d7d9fad34ab6d3d56ac
Emily Landgrebe <emilylandgrebe@Emilys-MacBook-Pro.local>
2019-07-31 17:01:54 -0500

    added jQuery

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('#add').on('click', function() {
+    let $input = $('input').val();
+    console.log($input);
+    $('.main').append(`<div id='text'><span id='box'>X</span>${$input}</div>`);
+
+    $('input').val('');
+})
