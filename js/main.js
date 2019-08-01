4f936c2a63696aa40310776031a1328aa98f7158
Emily Landgrebe <emilylandgrebe@Emilys-MacBook-Pro.local>
2019-07-31 20:41:52 -0500

    final draf of lab

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 $('#add').on('click', function() {
     let $input = $('input').val();
-    // console.log($input);
     $('.main').append(`<div id='text'><span id='box'>X</span>${$input}</div>`);
 
     $('input').val('');
 })
 
-$('.main').on('click', (function() {
+$('.main').on('click', function() {
     let $listText = $('#text');
     $listText.remove();
-}))
-
+})
